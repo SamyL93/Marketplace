284b0773f6150efc7fc101c7c8895685723e50aa
Samy Letombe <samy.letombe93@gmail.com>
2020-02-23 23:41:56 +0100

    Service Inscription

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Doctrine\ORM\EntityManager;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Twig\Environment;
+use App\Entity\User;
+use App\Form\InscriptionType;
+
+class SecurityController extends AbstractController
+{
+public function inscription (Request $request, EntityManagerInterface $manager, UserPasswordEncoderInterface $encoder) {
+    $user = new User();
+    $form = $this->createForm(InscriptionType::class, $user);
+    $form->handleRequest($request);
+    if($form->isSubmitted() && $form->isValid()){
+        $hash=$encoder->encodePassword($user,$user->getPassword());
+        $user->setPassword($hash);
+        $manager->persist($user);
+        $manager->flush();
+    }
+    return $this->render('security/inscription.html.twig', [
+        'form' => $form->createView()
+    ]);
+}
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @UniqueEntity(
+ *     fields={"mail"},
+ *     message="This email is already used"
+ * )
+ */
+class User implements UserInterface
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string")
+     * @Assert\Length(min="8",minMessage="your password must be at least 8 characters")
+     */
+    private $password;
+    /**
+     * @Assert\EqualTo(propertyPath="password",message="Password not equal")
+     */
+    private $confirm_password;
+
+    /**
+     * @ORM\Column(type="string")
+     * @Assert\Email()
+     */
+    private $mail;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function getMail(): ?string
+    {
+        return $this->mail;
+    }
+
+    public function getConfirmPassword(): ?string
+    {
+        return $this->confirm_password;
+    }
+
+    public function setUserName($name)
+    {
+        $this->username = $name;
+    }
+
+    public function setMail($mail)
+    {
+        $this->mail = $mail;
+    }
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    public function setConfirmPassword($confirm)
+    {
+        $this->confirm_password = $confirm;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getRoles()
+    {
+        // TODO: Implement getRoles() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getSalt()
+    {
+        // TODO: Implement getSalt() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function eraseCredentials()
+    {
+        // TODO: Implement eraseCredentials() method.
+    }
+}
