f983a0a10fec0efa5f1a34ed67a7d4b819f5829c
SamyL93 <samy.letombe93@gmail.com>
2020-02-27 23:40:12 +0100

    fusion about,contact ctrler

--- src/Controller/InfoController.php
+++ src/Controller/InfoController.php
@@ -9,15 +9,19 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
 
-class ContactController extends AbstractController
+class InfoController extends AbstractController
 {
+
+    public function about(Environment $twig)
+    {
+        $content = $twig->render('about.html.twig');
+        return new Response($content);
+    }
 
     public function contact(Environment $twig)
     {
-
         $content = $twig->render('contact.html.twig');
         return new Response($content);
-
     }
 }
 
