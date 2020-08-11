5519c31a7b99048b5879561a2657466b1061d369
Samy Letombe <samy.letombe93@gmail.com>
2020-08-12 00:37:04 +0200

    ajustements design

--- src/Migrations/Version20200811220037.php
+++ src/Migrations/Version20200811220037.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200322211930 extends AbstractMigration
+final class Version20200811220037 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,6 +22,8 @@ final class Version20200322211930 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('CREATE TABLE contact (id INT AUTO_INCREMENT NOT NULL, name TINYTEXT DEFAULT NULL, last_name TINYTEXT DEFAULT NULL, mail TINYTEXT DEFAULT NULL, subject LONGTEXT DEFAULT NULL, message LONGTEXT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE newsletter (id INT AUTO_INCREMENT NOT NULL, mail LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE user DROP date_naissance, DROP date_inscription');
     }
 
@@ -30,6 +32,8 @@ final class Version20200322211930 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('DROP TABLE contact');
+        $this->addSql('DROP TABLE newsletter');
         $this->addSql('ALTER TABLE user ADD date_naissance DATE DEFAULT NULL, ADD date_inscription DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
     }
 }
